ec9602a13350c06dda68b702f40f693f15e91a8e
Yusuf A <Askar9974@gmail.com>
2024-03-21 22:51:42 -0400

    created chart page

--- front-end/src/Chart.js
+++ front-end/src/Chart.js
@@ -1,15 +1,47 @@
-// ChartPage.js
-import React from "react";
-import ExchangeRateChart from "./ExchangeRateChart";
-import { historicalData } from "./data"; // Assuming you have historical data
+// ExchangeRateChart.js
+import React, { useEffect, useRef } from "react";
+import Chart from "chart.js/auto";
 
-const ChartPage = () => {
+const ExchangeRateChart = ({ historicalData }) => {
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    if (chartRef.current) {
+      const ctx = chartRef.current.getContext("2d");
+
+      new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: historicalData.labels,
+          datasets: [
+            {
+              label: "Exchange Rate Trends",
+              data: historicalData.data,
+              borderColor: "#00bcd4",
+              borderWidth: 2,
+              fill: false,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            x: {
+              type: "linear",
+              position: "bottom",
+            },
+          },
+        },
+      });
+    }
+  }, [historicalData]);
+
   return (
     <div>
-      <h1>Exchange Rate Chart</h1>
-      <ExchangeRateChart historicalData={historicalData} />
+      <canvas ref={chartRef} width="400" height="200" />
     </div>
   );
 };
 
-export default ChartPage;
+export default ExchangeRateChart;
